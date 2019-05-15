34b4a6c8b9668c290d55fb5b13ae4625043a7255
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-15 10:09:57 -0700

    passing random color class test

---
+++ src/components/RandomColor.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react';
+// import styles from './RandomColor.css';
+
+export default class RandomColor extends PureComponent {
+  state = {
+    color: '',
+    starter: new Date(),
+    img: ''
+  }
+  
+  randomHex = () => {
+    const colorHex = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return colorHex;
+  }
+
+  componentDidMount() {
+    this.starterTime = setInterval(() => {
+      this.setState({ color: this.randomHex(), starter: new Date(), img: '' });
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState === this.state.color) {
+      this.setState({ img: 'https://thumbs.gfycat.com/DemandingWelltodoGallinule-poster.jpg' });
+    }
+  }
+
+  componentWillUnmount() {
+    this.starterTime && clearInterval(this.starterTime);
+  }
+
+  render() {
+    const { color } = this.state;
+    const styles = {
+      background: color,
+      width: '40em',
+      height: '40em'
+    };
+
+    return <div className="square" style={styles}></div>;
+  }
+}
